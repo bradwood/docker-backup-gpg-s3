53a7d49a7a8c027fdff3390d61688a3f9186489d
Bradley Wood <git@bradleywood.com>
2019-02-06 16:10:54 +0000

    added non-DRY checks for environment variable presence

--- run.sh
+++ run.sh
@@ -3,8 +3,37 @@
 # Import GPG public keys
 gpg --import /keys/*
 
+# This is ugly and very un-DRY, but sh(1) is not bash(1). So we will live with
+# this as it's manageable, even if inelegant.
+
+if [[ -z "${CRON_INTERVAL}" ]]; then
+  echo "CRON_INTERVAL environment variable not set. Aborting."
+  exit 1
+fi
+
+if [[ -z "${GPG_RECIPIENT}" ]]; then
+  echo "GPG_RECIPIENT environment variable not set. Aborting."
+  exit 1
+fi
+
+if [[ -z "${S3_BUCKET_NAME}" ]]; then
+  echo "S3_BUCKET_NAME environment variable not set. Aborting."
+  exit 1
+fi
+
+if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
+  echo "AWS_ACCESS_KEY_ID environment variable not set. Aborting."
+  exit 1
+fi
+
+if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
+  echo "AWS_ACCESS_KEY_ID environment variable not set. Aborting."
+  exit 1
+fi
+
+
 # Create and install crontab file
-echo "$CRON_INTERVAL /backup.sh" >> /backup.cron
+echo "$CRON_INTERVAL /backup.sh" > /backup.cron
 
 crontab /backup.cron
 
